4c34c1e3ece3bf7c0ffd36edc45b172f3f5063ea
Ruslan <r.sevrukov@invokeinc.com>
2021-09-24 13:01:30 +0300

    main controller

--- app/controllers/cryptocurrency_controller.rb
+++ app/controllers/cryptocurrency_controller.rb
@@ -1,2 +1,25 @@
 class CryptocurrencyController < ApplicationController
+  def full_info
+    render json: RequesterHelper.currencies params[:cc_tickers]
+  rescue
+    render json: "unhandled error", status: 500
+  end
+
+  def specific_info
+    RequesterHelper.currencies params[:cc_tickers], params[:fields]
+  rescue
+    render json: "unhandled error", status: 500
+  end
+
+  def convert_cc_to_fiat
+    NomicsHelper.convert_cc_to_fiat params[:cc_ticker], params[:fiat_ticker], params[:amount]
+  rescue
+    render json: "unhandled error", status: 500
+  end
+
+  def convert_cc_to_cc
+    NomicsHelper.convert_cc_to_cc params[:cc_from_ticker], params[:cc_to_ticker], params[:amount]
+  rescue
+    render json: "unhandled error", status: 500
+  end
 end
