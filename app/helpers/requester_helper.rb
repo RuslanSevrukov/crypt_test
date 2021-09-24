9a557a73e19d8e0aeb94fd3c9b2705251fe9b420
Ruslan <r.sevrukov@invokeinc.com>
2021-09-24 13:02:55 +0300

    requester default values set

--- app/helpers/requester_helper.rb
+++ app/helpers/requester_helper.rb
@@ -4,9 +4,34 @@ module RequesterHelper
 
   headers = {
     key: ENV("NOMICS_KEY"),
-  }
+  }.freeze
   
-  def currencies tickers, fields
+  all_fields = [
+    "id",
+    "original_symbol",
+    "name",
+    "description",
+    "website_url",
+    "logo_url",
+    "blog_url",
+    "discord_url",
+    "facebook_url",
+    "github_url",
+    "medium_url",
+    "reddit_url",
+    "telegram_url",
+    "twitter_url",
+    "whitepaper_url",
+    "youtube_url",
+    "linkedin_url",
+    "block_explorer_url",
+    "bitcointalk_url",
+    "replaced_by",
+    "cryptocontrol_coin_id",
+    "used_for_pricing",
+  ].freeze
+
+  def currencies tickers, fields=all_fields
     JSON.parse(HTTParty.get(
       "/currencies", 
       headers: headers,
