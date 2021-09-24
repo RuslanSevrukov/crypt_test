36a2bbc342bdd132162d931bfb79c539a8b52164
Ruslan <r.sevrukov@invokeinc.com>
2021-09-24 12:30:20 +0300

    prettify nomics helper

--- app/helpers/nomics_helper.rb
+++ app/helpers/nomics_helper.rb
@@ -1,17 +1,13 @@
 module NomicsHelper
-  @@usd_cc_prices = {}
-
-  
-
   def convert_cc_to_another cc_from_ticker, cc_to_ticker, amount
     amount * conversion_multiplier cc_from_ticker, cc_to_ticker
   end
 
   def conversion_multiplier cc_from_ticker, cc_to_ticker
-    cc_to_usd cc_to_ticker / cc_to_usd cc_from_ticker
+    usd_price cc_to_ticker / usd_price cc_from_ticker
   end
   
-  def cc_to_usd cc_ticker
-    @@usd_cc_prices[cc_ticker] ||= # some request
+  def usd_price cc_ticker
+    RequesterHelper.fiat_price cc_ticker, "USD"
   end
 end
